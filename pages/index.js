af39701e620b9e4661edeb7c6757c64b1b5915a2
Kaku <akash.sharma@rtcamp.com>
2021-04-13 11:03:53 +0530

    Logo and Search update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
-import Head from 'next/head'
+import Head from 'next/head';
 import Avatar from '../components/Avatar';
-import {MicrophoneIcon, ViewGridIcon} from '@heroicons/react/solid'
+import {MicrophoneIcon, ViewGridIcon} from '@heroicons/react/solid';
+import {SearchIcon} from '@heroicons/react/outline';
 import Image from 'next/image';
 export default function Home() {
 	return (
@@ -30,8 +31,32 @@ export default function Home() {
 			</header>
 
 			{/*Body*/}
-			<form>
+			<form className="flex flex-col items-center
+			mt-44 flex-grow ">
+				<Image
+				src="https://www.google.co.uk/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+				height= {100}
+				width= {300}
+				/>
+				<div className="flex w-full nt-5
+				hover:shadow-lg focus-within:shadow-lg
+				max-w-md rounded-full border-gray-200
+				px-5 py-3 items-center
+				sm:max-w-xl lg:max-w-2xl">
+					<SearchIcon className="h-5 mr-3
+					text-gray-500" />
+					<input type="text"
+					className="focus:outline-none flex-grow" />
+					<MicrophoneIcon className="h-5 ml-3
+					text-gray-500" />
+				</div>
+				<div className="flex flex-col w-1/2 space-y-2
+				justify-center mt-8
+				sm:space-y-0 sm:flex-row sm:space-x-4 ">
+					<button className="btn">Google search </button>
 
+					<button className="btn">I'm feeling Lucky </button>
+				</div>
 			</form>
 			{/*Footer*/}
     	</div>
